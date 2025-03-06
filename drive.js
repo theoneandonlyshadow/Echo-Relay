b19a0329a5b7368245bb9c4c70e70455327d3d9c
Tyler7x1 <jpm022504@gmail.com>
2025-03-06 19:32:37 +0530

    Parrallelism for Express

--- drive.js
+++ drive.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const cluster = require('cluster');
+const numCPUs = require('os').availableParallelism();
+const process = require('process');
 const path = require('path');
 const http = require('http');
 const { HandlePostReceive, HandleGetById, HandleQuickReceive } = require('./public/controllers/Receive.js');
@@ -18,34 +21,51 @@ const mongoURI = process.env.MONGO_URI;
 
 connect(mongoURI);
 
-const wssServer = http.createServer();
-initializeWebSocketServer(wssServer);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.set('views', path.join(__dirname, 'public/views'));
-app.set('view engine', 'ejs');
+try {
+    if (cluster.isPrimary) {
+        console.log(`Primary ${process.pid} is running`);
 
-app.use(LinkLogger);
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+        cluster.on('exit', (worker) => {
+            console.log(`worker ${worker.process.pid} died`);
+        });
+    } else {
 
-app.use('/upload', require('./public/routes/uploadRouter.js'));
-app.use('/delete', require('./public/routes/deleteRouter.js'));
-app.use('/', require('./public/routes/pagesRouter.js'));
+        const wssServer = http.createServer();
+        initializeWebSocketServer(wssServer);
 
-app.post('/receive', HandlePostReceive);
-app.post('/', HandleDownload);
-app.get('/:fileId', HandleGetById);
-app.get('/q/:fileId', HandleQuickReceive);
+        app.use(express.static(path.join(__dirname, 'public')));
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.json());
+        app.set('views', path.join(__dirname, 'public/views'));
+        app.set('view engine', 'ejs');
 
-app.use(renderNotFound);
+        app.use(LinkLogger);
 
-monitorDeletion();
+        app.use('/upload', require('./public/routes/uploadRouter.js'));
+        app.use('/delete', require('./public/routes/deleteRouter.js'));
+        app.use('/', require('./public/routes/pagesRouter.js'));
 
-server.listen(PORT, () => {
-    console.log(`${info} ExpressRelay initialized at http://localhost:${PORT}`);
-});
+        app.post('/receive', HandlePostReceive);
+        app.post('/', HandleDownload);
+        app.get('/:fileId', HandleGetById);
+        app.get('/q/:fileId', HandleQuickReceive);
 
-wssServer.listen(WS_PORT, () => {
-    console.log(`${info} WebSocket initialized at ws://localhost:${WS_PORT}`);
-});
+        app.use(renderNotFound);
+
+        monitorDeletion();
+
+        server.listen(PORT, () => {
+            console.log(`${info} ExpressRelay initialized at http://localhost:${PORT}`);
+        });
+
+        wssServer.listen(WS_PORT, () => {
+            console.log(`${info} WebSocket initialized at ws://localhost:${WS_PORT}`);
+        });
+    }
+} catch (error) {
+    console.error(`Some error occurred: ${error}`);
+};
