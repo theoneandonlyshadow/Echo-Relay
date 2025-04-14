4c5db65ad23723a9759e0941e5902c50493362dd
Madhav Nair <119957011+theoneandonlyshadow@users.noreply.github.com>
2025-04-14 23:16:11 +0530

    Delete public directory

--- public/controllers/LoggerStyles.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = { 
-    info: chalk.blue.bold(`[INFO]`),
-    err: chalk.red.bold(`[ERR]`),
-    warn: chalk.yellow.bold(`[WARN]`),
-    succ: chalk.green.bold(`[SUCC]`)
-}
--- public/routes/deleteRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { HandleDelete } = require('../controllers/Delete.js');
-
-const router = express.Router();
-
-router.delete('/:fileId', HandleDelete);
-
-module.exports = router;
